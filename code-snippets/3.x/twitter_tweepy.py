b7071b43f3a863b6db05e065db5e210c4e19ada1
Nitin <26771985+nitinx@users.noreply.github.com>
2018-12-10 22:14:47 +0530

    New Twitter Client

---
+++ code-snippets/3.x/twitter_tweepy.py
@@ -0,0 +1,39 @@
+#from __future__ import absolute_import, print_function
+
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import json
+
+
+# Open KEY files
+with open('e:\\GitHub\\python\\keys\\twitter.key') as key_file_twitter:
+    key_twitter = json.load(key_file_twitter)
+
+consumer_key = key_twitter[0]['CONSUMER_KEY']
+consumer_secret = key_twitter[0]['CONSUMER_SECRET']
+access_token = key_twitter[0]['ACCESS_TOKEN_KEY']
+access_token_secret = key_twitter[0]['ACCESS_TOKEN_SECRET']
+
+
+class StdOutListener(StreamListener):
+    """ A listener handles tweets that are received from the stream.
+    This is a basic listener that just prints received tweets to stdout.
+    """
+    def on_data(self, data):
+        js = json.loads(data)
+        print(js)
+        print(js['text'])
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+
+if __name__ == '__main__':
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    stream = Stream(auth, l)
+    stream.filter(track=['basketball'])
