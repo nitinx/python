f95f5277735b5d8124626a07c35d2ad406e03e27
Nitin <26771985+nitinx@users.noreply.github.com>
2018-12-21 19:07:46 +0530

    Update twitter_jsontocsv.py

--- code-snippets/3.x/twitter_jsontocsv.py
+++ code-snippets/3.x/twitter_jsontocsv.py
@@ -3,33 +3,26 @@ import csv
 
 file_src = 'twitter_dump_trend.json'
 file_tgt = 'twitter_dump_trend.csv'
-tweet_write = []
+cnt = 0
+fieldnames = ['id', 'created_at', 'quote_count', 'reply_count', 'retweet_count', 'favorite_count', 'favorited',
+              'retweeted', 'filter_level', 'geo', 'place', 'coordinates', 'lang', 'source', 'text']
 
-# Open file
-for line in open(file_src, 'r', encoding='utf-8'):
-    tweet_read = json.loads(line)
-    #print(tweet_read)
+# Open file to write
+with open(file_tgt, 'w', newline='\n', encoding='utf-8') as line_write:
+    writer = csv.DictWriter(line_write, delimiter="|", fieldnames=fieldnames, restval='NA', quoting=csv.QUOTE_ALL,
+                            extrasaction='ignore')
+    writer.writeheader()
 
-    with open(file_tgt, 'w', newline='', encoding='utf-8') as line_write:
-        writer = csv.writer(line_write, delimiter="|", quoting=csv.QUOTE_ALL)
+    for line in open(file_src, 'r', encoding='utf-8'):
+        tweet_read = json.loads(line)
+        #print(tweet_read)
 
-        tweet_write.append(tweet_read['created_at'])
-        tweet_write.append(tweet_read['id'])
-        tweet_write.append(tweet_read['retweet_count'])
-        tweet_write.append(tweet_read['favorite_count'])
-        tweet_write.append(tweet_read['filter_level'])
-        tweet_write.append(tweet_read['geo'])
-        tweet_write.append(tweet_read['place'])
-        tweet_write.append(tweet_read['coordinates'])
-        tweet_write.append(str(tweet_read['lang']).encode('utf-8'))
-        tweet_write.append(str(tweet_read['text']).encode('utf-8'))
-        print(tweet_write)
+        cnt += 1
+        if cnt > 10000:
+            break
+        elif cnt % 100 == 0:
+            print(cnt)
+
+        tweet_write = tweet_read
+        tweet_write['text'] = str(tweet_write['text']).replace('\n', ' ')
         writer.writerow(tweet_write)
-
-        '''try:
-            tweet_write.append(str(tweet_read['lang']).encode('utf-8'))
-            tweet_write.append(str(tweet_read['text']).encode('utf-8'))
-            print(tweet_write)
-            writer.writerow(tweet_write)
-        except:
-            print("exception occurred!!")'''
